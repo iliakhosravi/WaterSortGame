26d342d018ec37c36555c0e2f2a1a50cb05fdea4
ilia828282 <ilia.khosravi82@gmail.com>
2023-12-31 13:30:34 +0330

    add swap and pour methods

--- WaterSort.py
+++ WaterSort.py
@@ -5,16 +5,31 @@ class Stack:
         self.items = []
 
     def is_empty(self):
-        return len(self.items) == 0
+        counter = 0
+        for i in range(len(self.items)):
+            if self.items[i] == "empty":
+                counter = counter + 1
+        if counter == len(self.items):
+            return True
+        return False
+    
+    def is_full(self):
+        counter = 0
+        for i in range(len(self.items)):
+            if self.items[i] != "empty":
+                counter = counter + 1
+        if counter == len(self.items):
+            return True
+        return False
 
     def push(self, item):
         self.items.append(item)
 
     def pop(self):
-        if not self.is_empty():
-            return self.items.pop()
-        else:
-            raise IndexError("pop from an empty stack")
+        # if not self.is_empty():
+        return self.items.pop()
+        # else:
+        #     raise IndexError("pop from an empty stack")
     
     def peek(self):
         if not self.is_empty():
@@ -62,7 +77,8 @@ class CircularLinkedList:
         current = self.head
         for _ in range(stack_index):
             current = current.next
-        return current.stack.pop()
+        current.stack.pop()
+        current.stack.push("empty")
     
     def get_node(self, stack_index):
         current = self.head
@@ -78,12 +94,28 @@ class CircularLinkedList:
         top = current.stack.items[-1]
         index = -1
         counter = 1
-        while top == "empty" and counter == current.stack.size():
+        while top == "empty" and counter != current.stack.size():
             index = index - 1
             counter = counter + 1
             top = current.stack.items[index]
 
         return top
+    
+    # all items are one color
+    def is_stack_complete(self, stack_index):
+        current = self.head
+        for _ in range(stack_index):
+            current = current.next
+        if current.stack.items[0] == "empty":
+            return False
+        for i in range(current.stack.size() - 1):
+            if current.stack.items[i] != current.stack.items[i+1]:
+                return False
+        
+        return True
+    
+    
+
         
 
     def display_stacks(self):
@@ -137,11 +169,11 @@ class WaterSortGame:
                 current = current.next
             print('\n')
 
-        # print(self.bottle.get_top_of_node(2))
+        # print(self.bottle.get_top_of_node(3))
         print("--------#-------------")
-        print(self.bottle.get_node(1))
-        print(self.select(1))
-        print(self.bottle.get_top_of_node(4))
+        # print(self.bottle.get_node(1))
+        # print(self.select(1))
+        # print(self.bottle.get_top_of_node(4))
 
 
     # ---------------------------- selection part ---------------------------------
@@ -190,11 +222,90 @@ class WaterSortGame:
     # --------------------------- pour part ---------------------------
 
     def pour(self, bottle_number):
-        pass
+        is_pour = True
+
+        if self.bottle_selected == -1:
+            return False
+        
+        # if self.select != True:
+        #     return False
+        
+        # selected bottle should not be empty
+        current = self.bottle.head
+        for _ in range(self.bottle_selected_number):
+            current = current.next
+        if current.stack.is_empty():
+            return False
+        
+
+        # destination bottle should not be full
+        current = self.bottle.head
+        for _ in range(bottle_number):
+            current = current.next
+        if current.stack.is_full():
+            return False
 
 
+        origin_bottle_number = self.bottle_selected_number
+
+        destination_bottle_number = bottle_number
+ 
+        temp_data = self.bottle.get_top_of_node(origin_bottle_number)
+        if current.stack.items[0] == "empty" or self.bottle.get_top_of_node(destination_bottle_number) == self.bottle.get_top_of_node(origin_bottle_number):
+            
+            counter = 0
+            for i in range(self.max_size_of_bottles - 1 , -1, -1):
+                if current.stack.items[i] == "empty":
+                    current.stack.pop()
+                    counter = counter + 1
+            self.bottle.push_to_stack(destination_bottle_number, temp_data)
+            for i in range(counter - 1):
+                self.bottle.push_to_stack(destination_bottle_number, "empty")
+
+            self.bottle.pop_from_stack(origin_bottle_number)
+            
+            is_pour = True
+
+        
+        return is_pour
+
+    # ------------------------------ swap bottle part -------------------------
+    def swap(self, bottle_number):
+        if self.bottle_selected == -1:
+            return False
+
+        current_selected = self.bottle.head
+        for _ in range(self.bottle_selected_number):
+            current_selected = current_selected.next
+
+        current_destination = self.bottle.head
+        for _ in range(bottle_number):
+            current_destination = current_destination.next
+
+        # Check if the selected bottle and destination bottle are not empty
+        # if current_selected.stack.is_empty() or current_destination.stack.is_empty():
+        #     return False
+
+        # Swap the contents of the selected bottle and destination bottle
+        temp = []
+        for i in range(self.max_size_of_bottles):
+            temp.append(current_destination.stack.items[i])
+        
+        for _ in range(self.max_size_of_bottles):
+            current_destination.stack.pop()
+
+        for i in range(self.max_size_of_bottles):
+            current_destination.stack.push(current_selected.stack.items[i])
+        
+        for _ in range(self.max_size_of_bottles):
+            current_selected.stack.pop()
+
+        for i in range(self.max_size_of_bottles):
+            current_selected.stack.push(temp[i]) 
     
-
+        return True
+        
+        
 
 
 
@@ -203,6 +314,15 @@ class WaterSortGame:
 def main():
     game = WaterSortGame(5, ['yellow', 'pink', 'blue'])
     game.display()
+    game.select(1)
+    # game.selectPrevious()
+    # game.pour(3)
+    game.swap(3)
+    
+    # game.select(2)
+    # game.pour(3)
+    game.display()
+
 
 
 
